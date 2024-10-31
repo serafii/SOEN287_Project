d25ee23aad6d35dbfbad6cdd4f994b216e55c2e0
SamiE <sami.erafii@gmail.com>
2024-10-31 17:50:01 -0400

    Final commit for deliverable 1

--- Home page/scripts.js
+++ Home page/scripts.js
@@ -1,9 +1,12 @@
     const title = document.getElementById("businessTitle");
     const slogan = document.getElementById("businessSlogan");
 
-    if(title===localStorage.getItem("title") && slogan ===localStorage.getItem("slogan")){
-    localStorage.setItem("title",title.textContent);
-    localStorage.setItem("slogan",slogan.textContent);
+    if (!localStorage.getItem("title")) {
+        localStorage.setItem("title", title.textContent);
+    }
+    if (!localStorage.getItem("slogan")) {
+        localStorage.setItem("slogan",slogan.textContent);
     }
     title.textContent = localStorage.getItem("title");
     slogan.textContent = localStorage.getItem("slogan");
+
