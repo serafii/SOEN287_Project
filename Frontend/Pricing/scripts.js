357fcb9b19162d32aae35abaae78cc0091d3a39c
SamiE <sami.erafii@gmail.com>
2024-11-19 08:36:25 -0500

    Service request links redirect to login page if user is not logged in

---
+++ Frontend/Pricing/scripts.js
@@ -0,0 +1,5 @@
+document.querySelectorAll("request-link").forEach(() => {   
+    if(localStorage.getItem("loggedIn")==false){
+        document.getElementById("request-link").href = "../Login page/SignIn.html";   
+    }
+});
--- Frontend/Services/scripts.js
+++ Frontend/Services/scripts.js
@@ -1,11 +1,3 @@
-if (localStorage.getItem("loggedIn") == "true") {
-    document.getElementById("login").classList.add("hidden");
-    document.getElementById("signUp").classList.add("hidden");
-    document.getElementById("signOut").classList.remove("hidden");
-    document.getElementById("dashboard").classList.remove("hidden");
-} else {
-    document.getElementById("login").classList.remove("hidden");
-    document.getElementById("signUp").classList.remove("hidden");
-    document.getElementById("signOut").classList.add("hidden");
-    document.getElementById("dashboard").classList.add("hidden");
+if(localStorage.getItem("loggedIn")==false){
+ document.getElementById("request-link").href = "../Login page/SignIn.html";   
 }
