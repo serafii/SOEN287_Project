c7e3b2bc2bc1bb4e73978cee91236b6f034515f3
SamiE <sami.erafii@gmail.com>
2024-11-21 15:13:39 -0500

    changed links for pricing/services if manager logged in

--- Frontend/Pricing/scripts.js
+++ Frontend/Pricing/scripts.js
@@ -1,5 +1,11 @@
 if(localStorage.getItem("state") === "manager"){
-    alert("You are not authorized to access this page, please log in as a user to access this page");
+    document.querySelectorAll(".request-link").forEach(link => {   
+            link.href = "";   
+            link.onclick = function(){
+                alert("You are not authorized to access this page, please log in as a user to access this page");
+            }
+        });
+
 }else{
         document.querySelectorAll(".request-link").forEach(link => {   
         if(localStorage.getItem("loggedIn") === "true"){
--- Frontend/Services/scripts.js
+++ Frontend/Services/scripts.js
@@ -1,6 +1,9 @@
 if(localStorage.getItem("loggedIn") === "true"){
     if(localStorage.getItem("state") === "manager"){
-        alert("You are not authorized to access this page, please log in as a user to access this page");
+        document.getElementById("request-link").href =  "";   
+        document.getElementById("request-link").onclick = function(){
+            alert("You are not authorized to access this page, please log in as a user to access this page");
+        }  
     }
     else
         document.getElementById("request-link").href =  "../Service Request/servicereq.html";   
