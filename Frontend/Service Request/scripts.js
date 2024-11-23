2c06cd58379ae555b90135869579ec514ee32b6b
Salia pona <sa_pona@live.concordia.ca>
2024-11-23 15:01:14 -0500

    changes for the date in service request/ can't request a service before the current dat

--- Frontend/Service Request/scripts.js
+++ Frontend/Service Request/scripts.js
@@ -1,5 +1,8 @@
 function addService() {
     $('#serviceRequestModal').modal('show');
+    const dateInput = document.getElementById('date');
+    const today = new Date().toISOString().split('T')[0];
+    dateInput.setAttribute('min', today); // Set the min attribute to today's date
 }
 
 document.getElementById('serviceRequestForm').addEventListener('submit', function(event) {
