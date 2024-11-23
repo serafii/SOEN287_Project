70d0aca97cd1e41575775e2302f87b11784338ae
Salia pona <sa_pona@live.concordia.ca>
2024-11-23 14:50:00 -0500

    made it impossible to choose a date prior to the current in the request service page/ changes in Frontend/Service Request script.js and servicereq.ejs

--- Frontend/Service Request/scripts.js
+++ Frontend/Service Request/scripts.js
@@ -1,14 +1,14 @@
 function addService() {
     $('#serviceRequestModal').modal('show');
+    const dateInput = document.getElementById('date');
+    const today = new Date().toISOString().split('T')[0];
+    dateInput.setAttribute('min', today); // Set the min attribute to today's date
 }
 
 document.getElementById('serviceRequestForm').addEventListener('submit', function(event) {
     // Handle form submission logic here
-    alert('Servicce added! Check your dashboard for updates.');
+    alert('Service added! Check your dashboard for updates.');
     $('#serviceRequestModal').modal('hide');
-
-
 });
 
 document.getElementById('username').value = localStorage.getItem('username');
-
