f642a85da360fd2102bc377b65a8a50e4581e4f3
SamiE <sami.erafii@gmail.com>
2024-11-22 23:39:32 -0500

    Changed alert message in my services for client

--- Frontend/Client dashboard/cancellation page/scripts.js
+++ Frontend/Client dashboard/cancellation page/scripts.js
@@ -10,7 +10,7 @@ function removeService(button){
 document.getElementById('username').value = localStorage.getItem('username');
 
    function confirmService(serviceId){
-     if(confirm("Are you sure you want to confirm that this service has been paid?") === true){
+     if(confirm("Are you sure you want to cancel this service?") === true){
        fetch('/cancelService', {
          method: 'POST',
          headers: {
