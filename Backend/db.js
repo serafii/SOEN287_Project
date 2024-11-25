31bdef47bd338fee56410577a0cfbef5cece2eb8
SamiE <sami.erafii@gmail.com>
2024-11-25 15:34:21 -0500

    Used connection pool

--- Backend/db.js
+++ Backend/db.js
@@ -1,13 +1,15 @@
 require('dotenv').config({path:'./routes/.env'}); 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER, 
     password: process.env.DB_PASS, 
     database: process.env.DB_NAME, 
     port: process.env.DB_PORT,
-    connectTimeout: 10000
+    connectionLimit: 5, //Clever cloud's free plan
+    waitForConnections: true,
+    queueLimit: 0
   });
 
   db.connect((err) => {
